1d4849f6fa970e928d0eea54b571b052d02ac7ed
Komal2000das <119794374+Komal2000das@users.noreply.github.com>
2024-01-27 00:24:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var main = document.querySelector("body")
+var crsr = document.querySelector(".cursor")
+
+main.addEventListener("mousemove", function(dets){
+crsr.style.left = dets.x +"px"
+crsr.style.top = dets.y +"px"
+})
